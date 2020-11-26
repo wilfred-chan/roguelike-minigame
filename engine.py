c710009913c06e936b01024b1ac69c4b5c9dc31a
WilfredChan <69495364+wilfred-chan@users.noreply.github.com>
2020-11-26 16:56:39 +0800

    Updated engine.py with class Entity

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import tcod as libtcod
+from entity import Entity
 from input_handlers import handle_keys
 
 
@@ -6,8 +7,9 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('resouces/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'Rogue-like Mini-game', False)
@@ -22,13 +24,13 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
         libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, '@', libtcod.BKGND_NONE)
         libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         # Next line will print your whole setup (e.g. 3 lines above)
         libtcod.console_flush()
         # Next line is pretty tricky.
         # It will be saved till next loop, and be printed by tcod.console_flush()
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)  # Override previous @
+        libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)  # Override previous @
 
         # Next line is deprecated
         # key = libtcod.console_check_for_keypress()
@@ -41,8 +43,7 @@ def main():
 
         if move:
             dx, dy = move  # unpack the tuples
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
