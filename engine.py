a24e12f82098a761a2ab2dcbf090ab6796fdcccd
WilfredChan <69495364+wilfred-chan@users.noreply.github.com>
2020-11-26 18:25:02 +0800

    Added map_objects package and defined class Tile

--- engine.py
+++ engine.py
@@ -8,6 +8,8 @@ from render_functions import render_all, clear_all
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
---
+++ map_objects/game_map.py
@@ -0,0 +1,26 @@
+from map_objects import Tile
+
+
+class GameMap:
+    def __init__(self, width, height):
+        """
+        docstring
+        """
+        self.width = width
+        self.height = height
+        self.tiles = self.initialize_tiles()
+
+    def initialize_tiles(self):
+        # Initialize a 2d array of width * height
+        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
+
+        # for demonstration purpose only
+        tiles[30][22].blocked = True
+        tiles[30][22].block_sight = True
+        tiles[31][22].blocked = True
+        tiles[31][22].blocked = True
+        tiles[32][22].block_sight = True
+        tiles[32][22].block_sight = True
+
+        return tiles
+
